9adec5f8ddc3a571de8b7e3a9ef918d9b246c130
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-04-21 16:52:34 +0100

    First commit

---
+++ Ch08TheDrill/Ch08TheDrill/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch08TheDrill
+//
+//  Created by Stefan Diaconu on 21/04/2019.
+//  Copyright © 2019 Stefan Diaconu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
