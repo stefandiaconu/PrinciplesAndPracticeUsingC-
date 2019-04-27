ea3d917142969178fbe882e8f79946f078b66956
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-04-28 00:01:05 +0100

    Ch08-05&06

--- Ch08-05/Ch08-05/main.cpp
+++ Ch08-05/Ch08-05/main.cpp
@@ -18,7 +18,24 @@ vector<int> reverseVector(vector<int> v1, vector<int> v2)
     return v2;
 }
 
+vector<string> reverseVector(vector<string> v1, vector<string> v2)
+{
+    int x = (int)v1.size()-1;
+    for (int i=x; i>=0; i--) {
+        v2.push_back(v1[i]);
+    }
+    return v2;
+}
+
 vector<int> swapVector(vector<int> v)
+{
+    for ( int i=0; i<v.size() / 2; i++) {
+        swap(v[i], v[(int)v.size()-1-i]);
+    }
+    return v;
+}
+
+vector<string> swapVector(vector<string> v)
 {
     for ( int i=0; i<v.size() / 2; i++) {
         swap(v[i], v[(int)v.size()-1-i]);
@@ -30,6 +47,9 @@ int main() {
     // insert code here...
     vector<int> v1 = {1, 3, 5, 7, 9};
     vector<int> v2;
+    vector<string> vs1 = {"a","b","c","d","e","f"};
+    vector<string> vs2;
+    
     v2 = reverseVector(v1, v2);
     for (int i=0; i<v2.size(); i++) {
         cout << v2[i] << "," << endl;
@@ -37,8 +57,22 @@ int main() {
     
     cout << endl;
     
+    vs2 = reverseVector(vs1, vs2);
+    for (int i=0; i<vs2.size(); i++) {
+        cout << vs2[i] << "," << endl;
+    }
+    
+    cout << endl;
+    
     v2 = swapVector(v1);
     for (int i=0; i<v2.size(); i++) {
         cout << v2[i] << "," << endl;
+    }
+    
+    cout << endl;
+    
+    vs2 = swapVector(vs1);
+    for (int i=0; i<vs2.size(); i++) {
+        cout << vs2[i] << "," << endl;
     }
 }
