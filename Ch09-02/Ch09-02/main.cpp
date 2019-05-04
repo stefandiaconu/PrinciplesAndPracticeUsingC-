2532a2ac1a3396969fbdd72db55099f2ca06d678
Stefan Diaconu <stefandiaconu@Stefans-MBP.broadband>
2019-05-04 22:11:57 +0100

    Ch09TheDrill

---
+++ Ch09-02/Ch09-02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch09-02
+//
+//  Created by Stefan Diaconu on 04/05/2019.
+//  Copyright © 2019 Stefan Diaconu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
