099c7fd67d96596117a7a0224047b8897342974e
stefandiaconu <stefan.diaconu@outlook.com>
2019-06-20 00:18:56 +0100

    Ch10-02

---
+++ Ch10-03/Ch10-03/main.cpp
@@ -0,0 +1,81 @@
+//
+//  main.cpp
+//  Ch10-03
+//
+//  Created by Stefan Diaconu on 19/06/2019.
+//  Copyright © 2019 Stefan Diaconu. All rights reserved.
+//
+
+#include "../../std_lib_facilities.h"
+
+struct Reading {
+    int hour;
+    double temperature;
+    Reading (int h, double t) : hour(h),temperature(t) {};
+    Reading () : hour(),temperature() {};
+};
+
+ostream& operator<<(ostream& os, const Reading& r) {
+    return os << '(' << r.hour << ',' << r.temperature << ')';
+}
+
+istream& operator>>(istream& is, Reading& r)
+{
+    int hour;
+    double temperature;
+    char ch1, ch2, ch3;
+    is >> ch1 >> hour >> ch2 >> temperature >> ch3;
+    if (!is) return is;
+    if (ch1 != '(' || ch2 != ',' || ch3 != ')') {
+        is.clear(ios_base::failbit);
+        return is;
+    }
+    r = Reading{hour,temperature};
+    return is;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    ofstream ost;
+    ost.open("raw_temps.txt",ios_base::out);
+    vector<Reading> raw_temps;
+    int n = 0;
+    while (n < 50) {
+        // trully random number
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(0,24);
+        int hour = dist(mt);
+        // trully random number
+        std::random_device rd2;
+        std::mt19937 mt2(rd2());
+        std::uniform_int_distribution<int> dist2(-43,107);
+        std::random_device rd3;
+        std::mt19937 mt3(rd3());
+        std::uniform_int_distribution<int> dist3(0,9);
+        double temperature = dist2(mt2) + dist3(mt3)/double(10);
+        ost << Reading{hour,temperature};
+        n++;
+    }
+    ost.close();
+    cout << n << endl;
+    
+    cout << endl;
+    
+    vector<Reading> temps;
+    ifstream ist;
+    ist.open("raw_temps.txt",ios_base::out);
+    for (int i=0; i<n; ++i) {
+        Reading r;
+        ist >> r;
+        temps.push_back(r);
+    }
+    ist.close();
+    
+    for (int i=0; i<temps.size(); ++i) {
+        cout << temps[i] << endl;
+    }
+    
+    return 0;
+}
