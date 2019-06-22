6043ead5ed9bd9c6f2f57a9b7958beaeda6b81fa
stefandiaconu <stefan.diaconu@outlook.com>
2019-06-23 00:15:44 +0100

    Ch10-09

---
+++ Ch10-09/Ch10-09/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Ch10-09
+//
+//  Created by Stefan Diaconu on 23/06/2019.
+//  Copyright © 2019 Stefan Diaconu. All rights reserved.
+//
+
+#include "../../std_lib_facilities.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    ifstream file1("mydata1.txt");
+    ifstream file2("mydata2.txt");
+    ofstream file3("mydata3.txt");
+    file3 << file1.rdbuf() << file2.rdbuf();
+    
+    return 0;
+}
